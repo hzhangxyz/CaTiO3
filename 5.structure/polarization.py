d303f17a0325a4f2b3bdbc3ce97682d3ba456a63
trumpet <zh970205@outlook.com>
2017-03-19 16:21:25 +0800

    calc-polarization

---
+++ 5.structure/polarization.py
@@ -0,0 +1,23 @@
+def polarization(data):
+    return [data[0][i]*4 +
+            data[1][i]*2 -
+            data[2][i]*2 -
+            data[3][i]*2 -
+            data[4][i]*2
+            for i in range(3)]
+
+def readfile(name):
+    with open(name,"r") as f:
+        d = f.read().split('\n')
+    if d[8][0] == " ":
+        data=map(lambda x:map(float,x.split()[:3]),d[8:13])
+    else:
+        data=map(lambda x:map(float,x.split()[:3]),d[9:14])
+    return data
+
+def main():
+    for i in "Ca Ti Ca-O Ti-O".split():
+        print i,polarization(readfile(i))
+
+if __name__=="__main__":
+    main()
